a7e53df3f603d2bfffc5c343c3830502998fede1
CYPRIEN <84139302+Cyprien-Gmd@users.noreply.github.com>
2021-06-15 22:09:07 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-/* Open */
+// OUVERTURE DE LA NAV
 function openNav() {
     document.getElementById("myNav").style.height = "100vh";
     document.getElementById("bars_btn").style.display = "none";
@@ -6,17 +6,16 @@ function openNav() {
     document.getElementById("cross_btn").style.zIndex = "2";
     document.getElementById("logo").style.zIndex = "2";
     document.getElementById("")
-
 }
 
-/* Close */
+// FERMETURE DE LA NAV AVEC LE X
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
     document.getElementById("bars_btn").style.display = "flex";
     document.getElementById("cross_btn").style.display = "none";
 }
 
-// FERMETURE DU MANU AU SCROLL
+// FERMETURE DU MENU AU SCROLL
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
